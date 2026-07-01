260b1bf1104ff1e00bbe12d23597d25d4ec5a13c
Kenil Gondaliya <kenilgondaliya960@gmail.com>
2026-07-01 13:36:56 +0530

    commit

---
+++ SpringRest/SpringRest/src/main/java/com/SpringRest/SpringRest/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.SpringRest.SpringRest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+}
---
+++ SpringRest/SpringRest/src/main/java/com/SpringRest/SpringRest/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.SpringRest.SpringRest.repository;
+
+import org.springframework.stereotype.Repository;
+//import com.SpringRest.SpringRest.dto.UserDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+//    List<UserDto> user = new ArrayList<>();
+}
